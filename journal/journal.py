ccb82ca517f65ed8604393694000870336b8c4a4
Pavel <asinskijp188@gmail.com>
2025-04-24 08:07:49 +0300

    create files for classes

---
+++ journal/journal.py
@@ -0,0 +1,39 @@
+"""
+стуркура записей в хранилище:
+{
+    all_titles: [<str>, ...],
+    last_changes: <time>,
+    journal:
+    {
+        date:
+        [
+        {
+            time: <time>
+            title: <str>
+            message: <str>
+        },
+        ...
+        ]
+    }
+}
+
+фильтрация происходит по:
+    - date (дата)
+    - title (заголовок)
+"""
+
+class Journal:
+    """
+    Объект хранения части данных и последних записей.
+    """
+    
+    def __init__(self) -> None:
+        
+        self.dates: list[] = []
+        self.titles: list[str]: []
+        self.ready_to_work = False
+        
+    def sutup(self) -> None:
+        
+            
+        

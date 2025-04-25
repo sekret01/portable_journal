4675109bf00f30f446017b6dbbd7bb8b96d663f1
Pavel <asinskijp188@gmail.com>
2025-04-25 11:50:13 +0300

    add func <read_command> in class Reader

--- journal/reader.py
+++ journal/reader.py
@@ -1,23 +1,34 @@
 import time
 
 
+_commands = []
+
 class Reader:
     """ Обработка введенного текста и направление на запись в журнал """
     
     def __init__(self):
         ...
        
-    def create_record(self) -> dict[] | None:
+    def read_record(self) -> dict | None:
         title: str = input("тема: ")
         message: str = ""
-        while (line := input("> "):
+        print("начало записи\n")
+        while line := input(""):
             message += line
-        
+        print("\nконец записи")
+
         _delete: str = input("Для отмены введите [yes]: ")
-        if _delete.lower() == "yes": return None
+        if _delete.lower() == "yes":
+            return None
         
         title = title or "запись"
         date = time.time()
         
         return {'date': date, 'title': title, 'message': message}
+
+    def read_command(self) -> str | None:
+        command = input("> ").lower()
+        if command in _commands:
+            return command
+        return None
         
